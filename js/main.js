0353f8e738b08077cf73a37e42f121c48af7c85b
Marco Politti <marcopolitti.br@gmail.com>
2020-07-07 07:55:48 -0300

    Incorporando bootstrap, hide/show, table, progressbar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 function consultaCep(){
+
+    $(".barra_progresso").show();
+    
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
     $.ajax({
@@ -6,15 +9,29 @@ function consultaCep(){
         type: "GET",
         success: function(response) {
             
-            console.log(response);
+            //console.log(response);
+
+            $("#titulo_cep").html("CEP " + response.cep);
 
             $("#logradouro").html(response.logradouro);
-            
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+
+            $(".cep").show();
+            $(".barra_progresso").hide();
+
+            //$("#logradouro").html(response.logradouro);             
             //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML     = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML         = response.uf;
+            //document.getElementById("bairro").innerHTML     = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML         = response.uf;
 
         }
     })
-}
+};
+
+$(function(){
+    $(".barra_progresso").hide();
+    $(".cep").hide();
+});
